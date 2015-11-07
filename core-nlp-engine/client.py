215db216e0f6fc9e987d7bd8fd136c9c4178d4c4
server-Overload <cs12b1041@iith.ac.in>
2015-11-07 18:51:37 +0530

    Review parsing done

--- core-nlp-engine/client.py
+++ core-nlp-engine/client.py
@@ -1,6 +1,7 @@
 import json
 from jsonrpc import ServerProxy, JsonRpc20, TransportTcpIp
 from pprint import pprint
+import re
 
 class StanfordNLP:
     def __init__(self):
@@ -11,5 +12,18 @@ class StanfordNLP:
         return json.loads(self.server.parse(text))
 
 nlp = StanfordNLP()
-result = nlp.parse("Hello world!  Today is so beautiful.")
-pprint(result)
+review = "The case pictured is a soft violet color, but the case cover I received was a dark purple. While I'm sure the quality of the product is fine, the color is very different."
+result = nlp.parse(review)
+
+count = 0
+temp_result = result['sentences']
+parse_length = len(temp_result)
+for i in xrange(parse_length):
+    sentence = temp_result[i]['text']
+    sentence = str(sentence)
+    searchObj = re.search(r'color', sentence, re.M|re.I)
+    if searchObj:
+        count += 1
+
+if count > 0:
+    print review
